248b55d66e741591acbdd4054e9a4d4877caa367
LuhPaglia <lucaspaglia.dev@gmail.com>
2022-12-16 17:20:49 -0800

    config for database

---
+++ data/config.php
@@ -0,0 +1,8 @@
+<?php
+    $baseName = "http://localhost/PHP/EducationSystem/";
+    $hostName = "localhost:3306";
+    $userName = "root";
+    $password = "root1234";
+    $dbName = "beavers";  
+    session_start();
+?>
