7d04df91c4806a6d01e4b5f2e6791ca3ce45bae1
Honey Lee <honeyrun5@naver.com>
2022-12-21 02:47:49 -0800

    password hashing

--- server/login.php
+++ server/login.php
@@ -15,33 +15,42 @@
         if($dbcon = $dbSrv->dbConnect()){
 
             if($role == "admin"){
-                $result = $dbSrv->select('admin_tb',null,['email'=>"'$email'",'password'=>"'$pass'"],'AND');
-                if($row = $result->fetch_assoc() != null) {
-                    $admin = new adminObj($row['admin_id'],$row['user_name'],$row['password'],$row['email'],$row['profile_url'],$row['birthday'],$row['address']);
-                    $_SESSION['logUser'] = $admin;
-                    $_SESSION['role'] = "admin";
-                    header("Location: ".$baseName.'adminPage.php');
-                    exit();
+                $select = "SELECT * FROM admin_tb WHERE email='$email'";
+                $result = $dbcon->query($select);
+                if($user = $result->fetch_assoc() != null) {
+                    if(password_verify($pass,$user['pass'])){
+                        $admin = new adminObj($row['admin_id'],$row['user_name'],$row['password'],$row['email'],$row['profile_url'],$row['birthday'],$row['address']);
+                        $_SESSION['logUser'] = $admin;
+                        $_SESSION['role'] = "admin";
+                        header("Location: ".$baseName.'adminPage.php');
+                        exit();
+                    }
                 }
             }
             else if($role == "teacher"){
-                $result = $dbSrv->select('teacher_tb',null,['email'=>"'$email'",'password'=>"'$pass'"],'AND');
-                if($row = $result->fetch_assoc() != null) {
-                    $teacher = new teacherObj($row['teacher_id'],$row['user_name'],$row['password'],$row['email'],$row['course_id'],$row['salary'],$row['birthday'],$row['address']);
-                    $_SESSION['logUser'] = $teacher;
-                    $_SESSION['role'] = "teacher";
-                    header("Location: ".$baseName.'studentMng.php');
-                    exit();
+                $select = "SELECT * FROM teacher_tb WHERE email='$email'";
+                $result = $dbcon->query($select);
+                if($user = $result->fetch_assoc() != null) {
+                    if(password_verify($pass,$user['pass'])){
+                        $teacher = new teacherObj($row['teacher_id'],$row['user_name'],$row['password'],$row['email'],$row['course_id'],$row['salary'],$row['birthday'],$row['address']);
+                        $_SESSION['logUser'] = $teacher;
+                        $_SESSION['role'] = "teacher";
+                        header("Location: ".$baseName.'studentMng.php');
+                        exit();
+                    }
                 }
             }
             else if($role == "student"){
-                $result = $dbSrv->select('student_tb',null,['email'=>"'$email'",'password'=>"'$pass'"],'AND');
-                if($row = $result->fetch_assoc() != null) {
-                    $student = new studentObj($row['student_id'],$row['user_name'],$row['password'],$row['email'],$row['course_id'],$row['teacher_id'],$row['birthday'],$row['address']);
-                    $_SESSION['logUser'] = $student;
-                    $_SESSION['role'] = "student";
-                    header("Location: ".$baseName.'gradeMngPage.php');
-                    exit();
+                $select = "SELECT * FROM student_tb WHERE email='$email'";
+                $result = $dbcon->query($select);
+                if($user = $result->fetch_assoc() != null) {
+                    if(password_verify($pass,$user['pass'])){
+                        $student = new studentObj($row['student_id'],$row['user_name'],$row['password'],$row['email'],$row['course_id'],$row['teacher_id'],$row['birthday'],$row['address']);
+                        $_SESSION['logUser'] = $student;
+                        $_SESSION['role'] = "student";
+                        header("Location: ".$baseName.'gradeMngPage.php');
+                        exit();
+                    }
                 }
             }
 
--- services/dbservices.php
+++ services/dbservices.php
@@ -42,28 +42,5 @@ class dbServices{
         return $result;
     }
 
-    function select($tbName,$fieldArray=null,$conditionArray=null,$operator=null) {
-        if($fieldArray!=null) {
-            $fields = implode(',',$fieldArray);
-        } else {
-            $fields = "*";
-        }
-        if($conditionArray!=null) {
-            $where = "WHERE ";
-            foreach($conditionArray as $key=>$value) {
-                $where .= "$key=$value";
-                if($key!=array_key_last($conditionArray)) {
-                    $where .= " $operator ";
-                }
-            }
-        } else {
-            $where = '';
-        }
-
-        $selectCmd = "SELECT $fields FROM $tbName $where";
-        $result = $this->dbcon->query($selectCmd);
-        return $result;
-    }
-
 }
 ?>
