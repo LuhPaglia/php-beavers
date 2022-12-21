142ee5b0466c21a01e1caa8f8bfc12ec2fe04d79
Honey Lee <honeyrun5@naver.com>
2022-12-21 02:58:21 -0800

    db close

--- server/login.php
+++ server/login.php
@@ -18,10 +18,11 @@
                 $select = "SELECT * FROM admin_tb WHERE email='$email'";
                 $result = $dbcon->query($select);
                 if($user = $result->fetch_assoc() != null) {
-                    if(password_verify($pass,$user['pass'])){
+                    if(password_verify($pass,$user['password'])){
                         $admin = new adminObj($row['admin_id'],$row['user_name'],$row['password'],$row['email'],$row['profile_url'],$row['birthday'],$row['address']);
                         $_SESSION['logUser'] = $admin;
                         $_SESSION['role'] = "admin";
+                        $dbcon->close();
                         header("Location: ".$baseName.'adminPage.php');
                         exit();
                     }
@@ -31,10 +32,11 @@
                 $select = "SELECT * FROM teacher_tb WHERE email='$email'";
                 $result = $dbcon->query($select);
                 if($user = $result->fetch_assoc() != null) {
-                    if(password_verify($pass,$user['pass'])){
+                    if(password_verify($pass,$user['password'])){
                         $teacher = new teacherObj($row['teacher_id'],$row['user_name'],$row['password'],$row['email'],$row['course_id'],$row['salary'],$row['birthday'],$row['address']);
                         $_SESSION['logUser'] = $teacher;
                         $_SESSION['role'] = "teacher";
+                        $dbcon->close();
                         header("Location: ".$baseName.'studentMng.php');
                         exit();
                     }
@@ -44,10 +46,11 @@
                 $select = "SELECT * FROM student_tb WHERE email='$email'";
                 $result = $dbcon->query($select);
                 if($user = $result->fetch_assoc() != null) {
-                    if(password_verify($pass,$user['pass'])){
+                    if(password_verify($pass,$user['password'])){
                         $student = new studentObj($row['student_id'],$row['user_name'],$row['password'],$row['email'],$row['course_id'],$row['teacher_id'],$row['birthday'],$row['address']);
                         $_SESSION['logUser'] = $student;
                         $_SESSION['role'] = "student";
+                        $dbcon->close();
                         header("Location: ".$baseName.'gradeMngPage.php');
                         exit();
                     }
@@ -56,6 +59,7 @@
 
         } else "DB connection problem";
 
+        $dbcon->close();
         header("Location: ".$baseName.'index.php?msg=2');
         exit();
 
